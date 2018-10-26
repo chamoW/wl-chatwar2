8eb07ee5c40a4509b03f17acb664de0b0766c870
Wladimir López <wlopez@todo1.com>
2018-10-26 16:11:58 -0500

    Se crea paginas en ionic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { LoginPageModule } from '../pages/login/login.module';
+import { ConversationPageModule } from '../pages/conversation/conversation.module';
+import { ProfilePageModule } from '../pages/profile/profile.module';
+import { AboutPageModule } from '../pages/about/about.module';
 
 @NgModule({
   declarations: [
@@ -18,6 +22,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    LoginPageModule,
+    ConversationPageModule,
+    ProfilePageModule,
+    AboutPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ConversationPage } from '../conversation/conversation';
+import { LoginPage } from '../login/login';
+import { AboutPage } from '../about/about';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +12,20 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  goToConversation() {
+    this.navCtrl.push(ConversationPage);
+
+  }
+
+  goToLogin() {
+    this.navCtrl.push(LoginPage);
+
+  }
+
+  goToAbout(){
+    this.navCtrl.push(AboutPage);
   }
 
 }
